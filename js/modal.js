fc88f079782acee56494259a0dd24ccf5acb159d
YuriiT2007 <0443622992@mail.ru>
2022-02-05 21:57:58 +0300

    +knopka

--- js/modal.js
+++ js/modal.js
@@ -14,15 +14,3 @@
 })();
 
 
-(() => {
-    const menuBtnRef = document.querySelector("[date-menu-button]");
-    const mobileMenuRef = document.querySelector("[data-menu");
-    menuBtnRef.addEventListener("click", () => {
-        const expanded = 
-            menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-
-        menuBtnRef.classList.toggle("is-open");
-        menuBtnRef.setAttribute("aria-expanded", !expanded);
-        mobileMenuRef.classList.toggle("is-open");
-    });
-})();
